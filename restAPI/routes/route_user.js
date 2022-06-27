236d3aa73fb0ad3743cd72eb65f6994fc21c10a2
BassamMutairi <mutairibassam@gmail.com>
2022-06-27 22:58:05 +0300

    added remove user api using username

--- restAPI/routes/route_user.js
+++ restAPI/routes/route_user.js
@@ -4,10 +4,26 @@ const router = express.Router();
 // Controller
 const controller_appraisal_modules = require("../controllers/controller_user");
 
+// isAdmin validation
+router.use("/remove", (req, res, next) => {
+    /**
+     *  user type validation should be from database
+     *
+     *  needs to call userAPI to get user details, then validate it's type,
+     *  in case it's an admin proceed with the request.
+     *
+     *  otherwise @return unauthorized
+     *
+     */
+    next();
+});
+
 router.get("/test", controller_appraisal_modules.testAPI);
 
 router.get("/", controller_appraisal_modules.usersAPI);
 
 router.get("/:id", controller_appraisal_modules.userAPI);
+
+router.delete("/remove", controller_appraisal_modules.removeUserAPI);
 
 module.exports = router;
